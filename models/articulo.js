60d9578a51693ee8aef26ffcfb9ff4e4bc892cf8
leodlvalle <leodlvalle@gmail.com>
2019-11-26 17:48:24 +0100

    ariculos

--- models/articulo.js
+++ models/articulo.js
@@ -8,6 +8,28 @@ const getAll = () => {
     });
 }
 
+const getByFanzineId = (pArticuloId) => {
+    return new Promise((resolve, reject) => {
+        db.query('select * from articulos where fanzineId = ?', [pArticuloId],
+            (err, rows) => {
+                if (err) reject(err);
+                resolve(rows);
+            });
+    })
+}
+
+const insert = ({ titulo, texto, imagen, fanzineId }) => {
+    return new Promise((resolve, reject) => {
+        db.query('insert into articulos (titulo, texto, imagen, fanzineId) values (?, ?, ?, ?)', [titulo, texto, imagen, fanzineId], (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        })
+    })
+
+}
+
 module.exports = {
-    getAll: getAll
+    getAll: getAll,
+    getByFanzineId: getByFanzineId,
+    insert: insert
 }
--- routes/api/articulos.js
+++ routes/api/articulos.js
@@ -9,6 +9,19 @@ router.get('/', async (req, res) => {
 });
 
 
+router.get('/:id', async (req, res) => {
+    const rows = await Articulo.getByFanzineId(req.params.id);
+    res.json(rows);
+    console.log(rows);
+})
+
+router.post('/create', async (req, res) => {
+    const result = await Articulo.insert(req.body);
+    console.log(req.body);
+    res.json()
+})
+
+
 
 
 module.exports = router;
